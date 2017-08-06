ccedcaaafa108111d1945fc1beb92876bde3e2c1
A GitHub User <kogam22@gmail.com>
2017-08-06 09:35:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-	res.send("Article Two Was Requested And Will Be Served Here");
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-	res.send("Article Three Was Requested And Will Be Served Here");
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
