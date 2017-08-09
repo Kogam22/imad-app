53a1fcc23e1a1a4734a9c261e43443573daffec6
A GitHub User <kogam22@gmail.com>
2017-08-09 18:13:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 //Change Text Of main-text
 element.innerHTML = "New Value";
+//Move The Image
+var img = document.getElementById("madi");
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
+
