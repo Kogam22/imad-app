d760985dc848a36aee2bdbebbf9ed0f8492c8c68
A GitHub User <kogam22@gmail.com>
2017-08-09 18:34:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 //Move The Image
 var img = document.getElementById("madi");
+var marginLeft=0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    var interval = setInterval(moveRight, 100);
   img.style.marginLeft = '100px';  
 };
 
