67c5224f4b53e2650c29853cb0581caf1cb9a8b5
A GitHub User <kogam22@gmail.com>
2017-08-09 22:41:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,6 +111,12 @@ app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
+var counter=0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
