1961a5727a1d11221672d5d9c29e9e5089dccd8a
A GitHub User <kogam22@gmail.com>
2017-08-09 23:44:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 var button = document.getElementById("counter");
 var counter = 0;
-button.onCLick = function() {
+button.oncLick = function() {
   
   //Make A Request To Counter Endpoint
   
