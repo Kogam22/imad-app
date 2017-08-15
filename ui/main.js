e91b8b76f2d9227caec482f7856dba396f84c3b3
A GitHub User <kogam22@gmail.com>
2017-08-15 17:42:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 function exec(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() 
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() 
     {
           if (request.readyState === XMLHttpRequest.DONE) 
           {
@@ -13,7 +15,25 @@ function exec(){
                 } 
           } 
     }; 
-    request.open("GET", "http://kogam22.imad.hasura-app.io/counter", true);
-    request.send(); 
-    console.log('EXECUTED');
+request.open("GET", "http://kogam22.imad.hasura-app.io/counter", true);
+request.send(); 
+console.log('EXECUTED');
 }
+
+//Capture Name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onClick = function ()
+{
+  //Make a Request To The Server And Send the name
+  //Capture a list of names and render it as a list
+  var names = [ "name1", "name2", "name3", "name4"];
+  var list = '';
+  for(var i=0; i < names.length; i++)
+  {
+    list += "<li>" + names[i] + "</li>";
+  }
+  var ul = document.getElementById("namelist");
+  ul.innerHTML = list;
+}; 
