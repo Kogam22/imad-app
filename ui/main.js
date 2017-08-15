356a1ef67e5923dcdd859c81dc0210c98fda27ce
A GitHub User <kogam22@gmail.com>
2017-08-15 19:43:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-function exec(){
+function exec()
+{
 
 var request = new XMLHttpRequest();
-
 request.onreadystatechange = function() 
     {
-          if (request.readyState === XMLHttpRequest.DONE) 
-          {
+        if (request.readyState === XMLHttpRequest.DONE) 
+            {
               //Take Some Action
               if (request.status === 200) 
                 { 
@@ -13,8 +13,8 @@ request.onreadystatechange = function()
                     var span = document.getElementById('count');
                     span.innerHTML = counter.toString();
                 } 
-          } 
-    }; 
+            } 
+}; 
 request.open("GET", "http://kogam22.imad.hasura-app.io/counter", true);
 request.send(); 
 console.log('EXECUTED');
