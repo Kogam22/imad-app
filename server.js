9e971f645360a0fa37104c4e3a3c83df6b47d25c
A GitHub User <kogam22@gmail.com>
2017-08-28 16:12:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,8 +131,10 @@ app.get('/test-db', function(req, res){
           if(err){
                     res.status(500).send(err, toString());
                  } 
-          else res.send(JSON.stringify(result));
-       });
+          else   {
+                    res.send(JSON.stringify(result));
+                 }
+        });
     
 });
 
