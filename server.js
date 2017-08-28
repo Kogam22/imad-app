58d5eefebdf31c2a075e97588da56ff84a579f2c
A GitHub User <kogam22@gmail.com>
2017-08-28 15:01:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: "kogam22",
+    database: "kogam22",
+    host: "db.hasura.imad-app.io",
+    port: "5432",
+    password: process.env.DB_PASSWORD
+};
+
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -110,6 +121,15 @@ function createTemplate (data) {
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+   //make a select request
+   //return a response with the results
+    
+});
+
+
 
 var counter = 0;
 app.get('/counter', function (req, res) {
