fb5e4335bd1e8011ed1eaa24c8ed5c40d9f84e80
A GitHub User <kogam22@gmail.com>
2017-08-31 12:59:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(morgan('combined'));
 var articles = {
      'article-one'   : {
     title: 'Article One - Shady Dude',
-    heading: 'Article One',
+    heading: 'ç',
     date: "Aug 5. 2017",
     content: `
             <p>
@@ -160,11 +160,30 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) 
+{
     //articleName == article-one
     //articles[articleName] == {} content object for article
+    
+    
+    pool.query("SELECT *FROM article WHERE title = " + req.params.articleName), function(err, result)
+    {
+        if(err) {
+                    res.status(500).send(err.toString());
+                }
+        
+        else if(result.rows.length === 0)   
+                {
+                    res.status(404).send("Article Not Found!");
+                }    
+        
+         else   {
+                    var articleData = result.rows[0];
+                    res.send(createTemplate(articleData));
+                }
+    };           
     var articleName = req.params.articleName;
-	res.send(createTemplate(articles[articleName]));
+	
 });
 
 app.get('/ui/main.js', function (req, res) {
