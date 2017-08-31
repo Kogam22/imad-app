de2d9cf20a9837ddcf1c8ace256e8b638563fb09
A GitHub User <kogam22@gmail.com>
2017-08-31 13:00:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article/:articleName', function (req, res) 
+app.get('/articles/:articleName', function (req, res) 
 {
     //articleName == article-one
     //articles[articleName] == {} content object for article
