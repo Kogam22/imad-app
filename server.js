741733d4cc42f4ac196233dbe667bd297be7eb35
A GitHub User <kogam22@gmail.com>
2018-02-26 21:11:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,162 +3,199 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
-var config ={
-	user: 'kogam22',
-	database: 'kogam22',
-	host: 'db.imad.hasura-app.io', 
-	port: '5432', 
-	password: process.env.DB_PASSWORD
+var config = {
+    user: "kogam22",
+    database: "kogam22",
+    host: "db.imad.hasura-app.io",
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
+
 
 
 var app = express();
 app.use(morgan('combined'));
 
-
- var articles = {
-	 'article-one' : {
-		title	 :  'Article One | Mohit' ,
-		date	 : 'Feb 17, 2018' , 
-		heading	:  'Article One',
-		content	 : `<p>
-		                    This is the content for Article one This is the ontent for Article one This is the ontent for Article one This is the ontent for Article one
-		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
-		                    
-		                </p>
-		            <p>
-		                    This is the content for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article oneThis is the ontent for Article one
-		            </p>`, 
-		},
-
-	 'article-two' : {
-			title	 :  'Article Two | Mohit' ,
-			date	 : 'Feb 18, 2018' , 
-			heading	:  'Article Two',
-			content	 : `<p>
-			                    This is the content for Second Article
-			            </p>`, 
-		},
-
-	 'article-three': {
-			title	 :  'Article Three | Mohit' ,
-			date	 : 'Feb 18, 2018' , 
-			heading	:  'Article Three',
-			content	 : `<p>
-			                    This is the content for Third Article
-			            </p>`, 
-		},
-
+var articles = {
+     'article-one'   : {
+    title: 'Article One - Shady Dude',
+    heading: 'Article One',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+                        },
+     
+     'article-two'   : {
+    title: 'Article Two - Shady Dude',
+    heading: 'Article Two',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+                        },
+     
+     'article-three' : {
+    title: 'Article Three - Shady Dude',
+    heading: 'Article Three',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+         
+                        },
 };
 
-var pool = new Pool(config);
+function createTemplate (data) {
 
-function createTemplate(data){
-	var title = data.title;
-	var date = data.date;
-	var heading = data.heading;
-	var content = data.content;
-	
-	var htmlTemplate = `
-	<html>
-	<head>
-	    <link href=ui/style.css rel="stylesheet"/>
-	        <title>
-	         ${title}
-	        </title>
-	    </head>
-	    <body>
-	        <div>
-	      <a href="/">Home</a>
-	    </div>
-	    <hr/>
-	    <h3>
-	        ${heading}
-	    </h3>
-	    <div>
-	        ${date}
-	    </div>
-	    <div>
-	    <p>
-	         ${content}
-	    </p>
-	    </div>
-	    </body>
-	</html>
-	`;
-	return htmlTemplate;
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" consent="width=device-width, initial-scale=1" />
+        <!--Above Line Is Used For Adjusting Width Of Screen According To Display-->
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date.toDateString()}
+            </div>
+            <div>
+               ${content}
+            </div>
+        </div>
+    </body>
+
+</html>
+                        `;
+    return htmlTemplate;
+
+
 }
 
-var counter=0;
-app.get('/counter', function (req, res) {
-	counter+=1;
-  res.send(counter.toString());
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
-// we have put the code of names here since article was executing first
- var names= [];
- app.get('/submit-name', function (req, res) { //url: submit-name?query regarding name
-   var name = req.query.name; //also provided by xpress framework
-   names.push(name);
-   //JSON 
-   res.send(JSON.stringify(names));
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+   //make a select request
+   //return a response with the results
+   pool.query('SELECT *FROM test', function(err, result) 
+       {
+          if(err){
+                    res.status(500).send(err.toString());
+                 } 
+                 
+          else   {
+                    res.send(JSON.stringify(result));
+                 }
+        });
+    
+});
+
+
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+      counter = counter + 1;
+      res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req, res) 
+{   //URL : /submit-name?name=xxxxxx
+    //Get The Names From Request Object
+    var name = req.query.name;
+    names.push(name);
+    // JSON : JavaScript Object Notation
+    res.send(JSON.stringify(names));
 });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
-app.get('/test-db',function(req,res)
+app.get('/articles/:articleName', function (req, res) 
 {
-	//make a query
-		//return a response
-	pool.query('select * from test',function(err,result) //Change res To result And Try
-    { 
-		if(err)
-        {
-			res.status(500).send(err.toString());
-		}
-		else
-        {
-			res.send(JSON.stringify(result.rows)); //Because You Are Using result Here
-		}
-	});
+    //articleName == article-one
+    //articles[articleName] == {} content object for article
+    
+    pool.query("SELECT *FROM article WHERE title = '" + req.params.articleName + "'", function(err, result)
+    {
+        if(err) {
+                    res.status(500).send(err.toString());
+                }
+        
+        else if(result.rows.length === 0)   
+                {
+                    res.status(404).send("Article Not Found!");
+                }    
+        
+         else   {
+                    var articleData = result.rows[0];
+                    res.send(createTemplate(articleData));
+                }
+    });           
 
-});
-
-/*app.get('/test-db',function(req,result){
-	//make a query
-		//return a response
-	pool.query('select * from test',function(err,res){
-		if(err){
-			res.status(500).send(err.toString());
-		}
-		else{
-			res.send(JSON.stringify(result.rows));
-		}
-	});
-});*/
-
-app.get('/:articleName', function (req, res){
- //aticleName == article-one
- //(articles[articleName]) == thw {} object of article one
- var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName])); 
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+	
 });
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
